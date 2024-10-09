b618eacc04a51d94dc6909dd5e1c47da2a1fa29c
Pavan <pavan.more1@gmail.com>
2024-10-09 20:41:38 +0530

    Initial Commit

---
+++ BankingApp/BankingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BankingAppApp.swift
+//  BankingApp
+//
+//  Created by Pavan More on 09/10/24.
+//
+
+import SwiftUI
+
+@main
+struct BankingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
