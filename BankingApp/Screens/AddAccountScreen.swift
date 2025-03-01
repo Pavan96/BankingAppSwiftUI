c0489a4fa66bed9f823e57644a649b15b52dfb70
Pavan <pavan.more1@gmail.com>
2025-03-02 01:56:28 +0530

    Updated api integration and data display

---
+++ BankingApp/Screens/AddAccountScreen.swift
@@ -0,0 +1,20 @@
+//
+//  AddAccountScreen.swift
+//  BankingApp
+//
+//  Created by Pavan More on 11/01/25.
+//
+
+import SwiftUI
+
+struct AddAccountScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AddAccountScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        AddAccountScreen()
+    }
+}
